fe2056faee9dc6c601bb9cb83e80c50a144618e7
Yago Mar <yagofmar@outlook.com>
2026-03-14 10:32:50 -0300

    add-filtros

--- Dashboard.py
+++ Dashboard.py
@@ -15,9 +15,28 @@ def formata_numero(valor, prefixo = ''):
 st.title('DASHBOARD DE VENDAS')
 
 url = 'https://labdados.com/produtos'
-response = requests.get(url)
+regiao = ['Brasil', 'Centro-Oeste', 'Nordeste', 'Norte', 'Sudeste', 'Sul']
+
+st.sidebar.title('Filtros')
+regiao = st.sidebar.selectbox('Região', regiao)
+
+if regiao == 'Brasil':
+    regiao = ''
+
+todos_anos = st.sidebar.checkbox('Dados de todo o período', value = True)
+if todos_anos:
+    ano = ''
+else:
+    ano = st.sidebar.slider('Ano', 2020, 2023)
+
+query_string = {'regiao':regiao.lower(), 'ano':ano}
+response = requests.get(url, params = query_string)
 dados = pd.DataFrame.from_dict(response.json())
 dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format="%d/%m/%Y")
+
+filtro_vendedores = st.sidebar.multiselect('Vendedores', dados['Vendedor'].unique())
+if filtro_vendedores:
+    dados = dados[dados['Vendedor'].isin(filtro_vendedores)]
 
 ## Tabelas
 ### Tabelas de Receitas
